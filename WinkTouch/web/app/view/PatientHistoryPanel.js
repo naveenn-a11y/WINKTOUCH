fe57e866abe6a85b37013288f21eec2ae113aacd
Christian Mokbel <chris@pointofviewsoftware.com>
2015-03-31 15:34:36 -0400

    entering a number without the s, will not look for the serial number; continued working of the wink tablet

--- WinkTouch/web/app/view/PatientHistoryPanel.js
+++ WinkTouch/web/app/view/PatientHistoryPanel.js
@@ -8,7 +8,6 @@
 Ext.define('WINK.view.PatientHistoryPanel', {
     extend: 'Ext.Panel',
     alias: 'widget.InvoicePanel',
-
     requires: [
         'WINK.store.PatientHistoryStore',
         'WINK.model.PatientHistory',
@@ -17,63 +16,110 @@ Ext.define('WINK.view.PatientHistoryPanel', {
         'Ext.Button',
         'Ext.SegmentedButton',
         'Ext.Label',
-        'Ext.tab.Bar'
+        'Ext.tab.Bar',
+        'Ext.Panel',
     ],
-
     config: {
         layout: 'hbox',
         scrollable: true,
         layout: 'hbox',
-        items: [
-            {
-                docked: 'top',
-                xtype: 'toolbar',
-                title: 'Christian Mokbel',
                 items: [
                     {
-                        text: 'Back',
-                        ui: 'back',
-                        action: 'goToMainScreen'
-                    },
-                    {
-                        xtype: 'spacer'
-                    },
-                    {
-                        text: 'New',
-                        ui: 'confirm',
-                        action : 'doNewPatient'
-                    },
-                    {
-                        text: 'Open',
-                        ui: 'forward',
-                        listeners: {
-                            tap: function(c) {
+                        docked: 'top',
+                        xtype: 'toolbar',
+                        title: 'Christian Mokbel',
+                        items: [
+                            {
+                                text: 'Back',
+                                ui: 'back',
+                                action: 'goToMainScreen'
+                            },
+                            {
+                                xtype: 'spacer'
+                            },
+                            {
+                                text: 'New',
+                                ui: 'confirm',
+                                listeners: {
+                                    tap: function(c) {
 
-                                alert('Open');
+                                        var myOverlay = Ext.create('Ext.Panel', {
+                                            modal: true,
+                                            hideOnMaskTap: true,
+                                            showAnimation: {
+                                                type: 'popIn',
+                                                duration: 250,
+                                                easing: 'ease-out'
+                                            },
+                                            hideAnimation: {
+                                                type: 'popOut',
+                                                easing: 'ease-out',
+                                                duration: 250
+                                            },
+                                            width: '200px',
+                                            height: '300px',
+                                            scrollable: true,
+                                            layout: 'vbox',
+                                            items: [
+                                                {
+                                                    docked: 'top',
+                                                    xtype: 'toolbar',
+                                                    title: 'New'
+                                                },
+                                                {
+                                                    xtype: 'list',
+                                                    indexBar: false,
+                                                    grouped: false,
+                                                    pinHeaders: true,
+                                                    flex:1,
+                                                    itemTpl: '{title}',
+                                                    data: [
+                                                        {title: 'Exam'},
+                                                        {title: 'Invoice'},
+                                                        {title: 'Attachment'},
+                                                        {title: 'Photo'}
+                                                    ]
 
+                                                }
+                                            ]
+
+
+                                        });
+                                        Ext.Viewport.add(myOverlay);
+                                        myOverlay.showBy(c);
+                                    }
+                                }
+                            },
+                            {
+                                text: 'Open',
+                                ui: 'forward',
+                                listeners: {
+                                    tap: function(c) {
+
+                                        alert('Open');
+                                    }
+                                }
                             }
-                        }
+                        ]
+
+                    },
+                    {
+                        xtype: 'list',
+                        width: '200px',
+                        indexBar: false,
+                        grouped: true,
+                        pinHeaders: true,
+                        store: 'PatientHistoryStore',
+                        itemTpl: '{Label}'
+                    },
+                    {
+                        xtype: 'container',
+                        flex: 1
                     }
                 ]
 
-            },
-            {
-                xtype:'list',
-                width:'200px',
-                indexBar: false,
-                grouped: true,
-                pinHeaders: true,
-                store: 'PatientHistoryStore',
-                itemTpl: '{Label}'
-            },
-            {
-                xtype:'container',
-                flex:1
-            }
-        ]
-       
     }
 
-  
+
 
 });
