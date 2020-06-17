4fbb81deab1e69c55d07dcf34d800470f43b6de2
unknown <walid@downloadwink.com>
2020-06-17 17:15:53 -0400

    EHR-660 return Json containing referral result after saving

--- WinkTouch/js/Referral.js
+++ WinkTouch/js/Referral.js
@@ -138,7 +138,8 @@ export class ReferralScreen extends Component<ReferralScreenProps, ReferralScree
     const visit: Visit = this.props.navigation.state.params.visit;
     let body : {} = {
         'htmlReferral': referralHtml,
-        'visitId': stripDataType(visit.id)
+        'visitId': stripDataType(visit.id),
+         'doctorId': 1 // To be replaced with the current selected doctor
       };
 
       let response = await fetchWinkRest('webresources/template/save/'+this.state.template, parameters, 'POST', body);
