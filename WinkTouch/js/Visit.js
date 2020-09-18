54f192a90252e35ee9b3a9c7b52779ba51b0f921
unknown <walid@downloadwink.com>
2020-09-18 16:05:22 -0400

    EHR-816 fix Referral crashing when opening visit from an Appointment

--- WinkTouch/js/Visit.js
+++ WinkTouch/js/Visit.js
@@ -369,6 +369,7 @@ class SectionTitle extends PureComponent {
 
 class VisitWorkFlow extends Component {
     props: {
+        patientInfo: PatientInfo,
         visitId: string,
         navigation: any,
         appointmentStateKey: string,
@@ -403,7 +404,7 @@ class VisitWorkFlow extends Component {
         const params = this.props.navigation.state.params; 
       
       if(params && params.refreshFollowUp) {
-        const patientInfo: PatientInfo =  this.props.navigation.state.params.patientInfo;
+        const patientInfo: PatientInfo =  this.props.patientInfo ? this.props.patientInfo : this.props.navigation.state.params.patientInfo;
         this.props.navigation.setParams({refreshFollowUp: false});
         await fetchReferralFollowUpHistory(patientInfo.id);
       }
@@ -638,6 +639,7 @@ class VisitWorkFlow extends Component {
     }
 
     renderActionButtons() {
+      const patientInfo: PatientInfo =  this.props.patientInfo ? this.props.patientInfo : this.props.navigation.state.params.patientInfo;
       return <View style={{paddingTop: 30*fontScale, paddingBottom:100*fontScale}}>
           <View style={styles.flow}>
             {this.state.visit.prescription.signedDate && <Button title={strings.signed} disabled={true}/>}
@@ -647,7 +649,7 @@ class VisitWorkFlow extends Component {
             {this.hasFinalClFitting() && <Button title={strings.printClRx} onPress={() => {printClRx(this.props.visitId)}}/>}
             {this.canTransfer() && <Button title={strings.transferRx} onPress={() => {transferRx(this.props.visitId)}}/>}
             <Button title={strings.printPatientFile} onPress={() => {printPatientFile(this.props.visitId)}}/>
-            {isReferralsEnabled() && <Button title={strings.referral} onPress={() => {this.props.navigation.navigate('referral', {visit:  getCachedItem(this.props.visitId), patientInfo: this.props.navigation.state.params.patientInfo, followUpStateKey: this.props.navigation.state.key})}}/>}
+            {isReferralsEnabled() && <Button title={strings.referral} onPress={() => {this.props.navigation.navigate('referral', {visit:  getCachedItem(this.props.visitId), patientInfo: patientInfo, followUpStateKey: this.props.navigation.state.key})}}/>}
             {!this.state.locked && !this.props.readonly && <Button title={strings.endVisit} onPress={() => this.endVisit()}/>}
         </View>
       </View>
@@ -1011,7 +1013,7 @@ export class VisitHistory extends Component {
             {this.state.selectedId===undefined && this.renderSummary()}
             {this.state.selectedId==='followup' && this.renderFollowUp()}
 
-            {this.state.selectedId && this.state.selectedId.startsWith('visit') && <VisitWorkFlow patientId={this.props.patientInfo.id}
+            {this.state.selectedId && this.state.selectedId.startsWith('visit') && <VisitWorkFlow patientInfo={this.props.patientInfo}
                 visitId={this.state.selectedId}
                 navigation={this.props.navigation}
                 appointmentStateKey={this.props.appointmentStateKey}
