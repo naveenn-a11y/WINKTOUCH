2563575d64d76304380bb347dc76049a5b3274af
zilicon <samuel@yuwe.be>
2021-01-07 15:16:43 -0500

    npm install pod install and build (using fixed pdflib branch from github)

--- WinkTouch/js/PatientFormHtml.js
+++ WinkTouch/js/PatientFormHtml.js
@@ -861,8 +861,7 @@ async function renderImage(
   }
 
   if (filePath) {
-    const requiredImage =
-      isWeb && image.startsWith('./image') ? require(`${filePath}`) : filePath;
+    const requiredImage = filePath;
     let imageValue: string = `<img src="${requiredImage}" border="1" style="width: ${style.width}pt; height:${style.height}pt; object-fit: contain; border: 1pt"/>`;
     if (!isWeb && image.startsWith('./image')) {
       const base64Image = await getBase64Image(image);
