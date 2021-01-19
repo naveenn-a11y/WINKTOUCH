5b252f3222204dc494aba690695e9c11cc8281da
winkdevshree <winkdevshree@users.noreply.github.com>
2021-01-19 15:00:49 -0500

    EW-34: Fixed null pointer expection for exam history screen when visited from diagnosis screen.

--- WinkTouch/js/Exam.js
+++ WinkTouch/js/Exam.js
@@ -555,8 +555,8 @@ export class ExamHistoryScreen extends Component {
         groupDefinition = deepClone(groupDefinition);
         groupDefinition.multiValue = false;
         return value.map((childValue: any, index: number) => {
+          const exam : Exam = this.props.navigation.state.params.exam;
           if (groupDefinition.type === 'SRx') {
-            let exam: Exam = this.props.navigation.state.params.exam;
             return (
               <GlassesDetail
                 title={formatLabel(groupDefinition)}
@@ -579,6 +579,7 @@ export class ExamHistoryScreen extends Component {
                 patientId={
                   this.state.patient ? this.state.patient.id : undefined
                 }
+                examId={exam.id}
               />
             );
           }
