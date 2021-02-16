1b72a94c61dafe3c8e835bf89301953065435a6a
zilicon <samuel@yuwe.be>
2021-02-16 11:35:01 -0500

    isWeb not needed in .web files

--- WinkTouch/index.js
+++ WinkTouch/index.js
@@ -2,13 +2,9 @@
  * @format
  */
 
-import {AppRegistry, Platform} from 'react-native';
+import {AppRegistry} from 'react-native';
 import Index from './js/Index';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Index);
-if (Platform.OS === 'web') {
-  AppRegistry.runApplication(appName, {
-    rootTag: document.getElementById('root'),
-  });
-}
+
--- WinkTouch/index.web.js
+++ WinkTouch/index.web.js
@@ -1,12 +1,9 @@
-import {AppRegistry, Platform} from 'react-native';
+import {AppRegistry} from 'react-native';
 import Index from './js/Index';
 import {name as appName} from './app.json';
 import {setIconsConfig} from './web/Util';
 
 AppRegistry.registerComponent(appName, () => Index);
-if (Platform.OS === 'web') {
-  setIconsConfig();
-  AppRegistry.runApplication(appName, {
-    rootTag: document.getElementById('root'),
-  });
-}
+setIconsConfig();
+AppRegistry.runApplication(appName, {rootTag: document.getElementById('root')});
+
