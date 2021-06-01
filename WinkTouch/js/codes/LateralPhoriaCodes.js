3c2000dfca974d79aa7845851ac73cba33102d58
unknown <walid@downloadwink.com>
2021-06-01 11:20:35 -0400

    EHR-984 update codes

--- WinkTouch/js/codes/LateralPhoriaCodes.js
+++ WinkTouch/js/codes/LateralPhoriaCodes.js
@@ -4,9 +4,9 @@
 'use strict';
 
 export const lateralPhoriaCodes = [
-  {code: 'Esophoria'},
-  {code: 'Exophoria'},
-  {code: 'Alternating Esophoria'},
-  {code: 'Alternating Exophoria'},
-  {code: 'Intermittent Alternating'},
+  {code: ' Esophoria'},
+  {code: ' Exophoria'},
+  {code: ' Alternating Esophoria'},
+  {code: ' Alternating Exophoria'},
+  {code: ' Intermittent Alternating'},
 ];
