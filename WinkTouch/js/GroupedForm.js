61d13e7b1bc9e1a0261a24dbee5eb6360696b470
unknown <walid@downloadwink.com>
2021-11-10 13:23:01 -0500

    EHR-1204 support dynamic field isVisible

--- WinkTouch/js/GroupedForm.js
+++ WinkTouch/js/GroupedForm.js
@@ -104,10 +104,15 @@ function getIsVisible(item: ?any, groupDefinition: GroupDefinition): ?{} {
     }
     const keyIdentifier: string[] = key.split('.');
     if (keyIdentifier[0] === 'visit') {
-      const visit: Visit = getCachedItem(item);
+      let visit: Visit = undefined;
+      if (item.startsWith('visit-')) {
+        visit = getCachedItem(item);
+      } else {
+        const exam: Exam = getCachedItem(item);
+        visit = exam !== undefined ? getCachedItem(exam.visitId) : undefined;
+      }
       const value: any =
         visit !== undefined ? visit[`${keyIdentifier[1]}`] : undefined;
-
       return reverseFlag ? isEmpty(value) : !isEmpty(value);
     } else {
       const exam: Exam = getCachedItem(item);
@@ -989,6 +994,7 @@ export class GroupedCard extends Component {
           this.props.exam,
         );
         let formattedValue = formatFieldValue(fieldValue, fieldDefinition);
+        if (!getIsVisible(this.props.exam.visitId, fieldDefinition)) return '';
         if (formattedValue === '') return '';
         if (cardRowFields.length === 1) {
           //Add the label for single field rows
@@ -1154,13 +1160,19 @@ export class GroupedForm extends Component {
         fieldDefinition,
       );
     }
-    const value = this.props.form
+    let value = this.props.form
       ? column
         ? this.props.form[column]
           ? this.props.form[column][fieldDefinition.name]
           : undefined
         : this.props.form[fieldDefinition.name]
       : undefined;
+    value =
+      value === undefined
+        ? fieldDefinition.defaultValue
+          ? fieldDefinition.defaultValue
+          : undefined
+        : value;
 
     //if (fieldDefinition.mappedField) {
     //  value = getExamFieldValue(fieldDefinition.mappedField, getCachedItem(this.props.examId));
@@ -1239,6 +1251,8 @@ export class GroupedForm extends Component {
   }
 
   renderFieldsRow(fieldDefinition: FieldDefinition) {
+    if (this.getIsVisible(fieldDefinition) === false) return null;
+
     let fields: any[] = [];
     const row: string[] = this.props.definition.rows.find(
       (row: string[]) =>
@@ -1412,6 +1426,7 @@ export class GroupedForm extends Component {
   }
 
   renderColumnedRows(columnDefinition: GroupDefinition) {
+    if (this.getIsVisible(columnDefinition) === false) return null;
     let rows: any[] = [];
     rows.push(this.renderColumnsHeader(columnDefinition));
     const columnedFields: FieldDefinition[] = columnDefinition.fields;
