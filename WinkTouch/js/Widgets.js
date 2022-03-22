ce9da12b631225b346e9a42f4206dda4c4b2dfcf
riadAlnajjar <>
2022-03-22 10:02:09 +0200

    Update Widgets.js

--- WinkTouch/js/Widgets.js
+++ WinkTouch/js/Widgets.js
@@ -1727,7 +1727,7 @@ export class ListField extends Component {
 
   renderPopup() {
     return (
-      <TouchableWithoutFeedback onPress={this.cancelEdit}>
+      <TouchableWithoutFeedback onPress={isWeb ? undefined : this.cancelEdit}>
         <View style={styles.popupBackground}>
           <Text style={styles.modalTitle}>
             {this.props.label}: {this.state.editedValue}
