8027689a060154ef7f00e7925d3df8e9150190f4
methodmain2 <olorunfemi@downloadwink.com>
2022-10-06 10:32:04 +0100

    EHR-1514 feat: show logged in user and location

--- WinkTouch/js/MenuBar.js
+++ WinkTouch/js/MenuBar.js
@@ -34,6 +34,7 @@ import {REACT_APP_HOST} from '../env.json';
 import {getCachedItem} from './DataCache';
 import {getPrivileges} from './Rest';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {ProfileMenu} from './Profile';
 
 export class Notifications extends PureComponent {
   render() {
@@ -169,7 +170,7 @@ export class MenuBar extends PureComponent {
     const hasConfig: boolean = getPhoropters().length > 1;
     return (
       <View style={styles.sideMenu}>
-        <Image source={require('./image/menulogo.png')} />
+        <ProfileMenu />
         {!noAccessAppointment && (
           <Button
             title={strings.calendar}
---
+++ WinkTouch/js/Profile.js
@@ -0,0 +1,112 @@
+/**
+ * @flow
+ */
+'use strict';
+import React, {Component} from 'react';
+import {
+    View,
+    TouchableWithoutFeedback,
+    TouchableOpacity,
+    Text,
+} from 'react-native';
+import { Avatar, Menu, Button as PaperButton } from 'react-native-paper';
+import {fontScale, styles} from './Styles';
+import {getStore, getDoctor} from './DoctorApp';
+
+
+String.prototype.capitalizedFirstLetter = function(){
+    return this.length > 0 ? this.charAt(0).toUpperCase() : '';
+};
+
+export class ProfileHeader extends Component {
+    render() {
+        const store = getStore();
+        const doctor = getDoctor();
+        
+        return (
+            <View style={{ padding: 20 * fontScale}}>
+                <Text style={{color: '#1db3b3', fontWeight: 'bold', marginBottom: 5 * fontScale}}>
+                    {`Welcome, ${doctor.firstName} ${doctor.lastName}`}
+                </Text>
+                <Text style={{color: 'gray'}}>{`${store.name} ${store.city}`}</Text>
+            </View>
+        );
+    };
+ }
+
+export const ProfileAvatar = () => {
+    const doctor = getDoctor();
+    const doctorInitials = `${doctor.firstName.capitalizedFirstLetter()}${doctor.lastName.capitalizedFirstLetter()}`;
+
+    return (
+        <View>
+            <Avatar.Text 
+                style={{backgroundColor: '#fff'}}
+                size={100 * fontScale} 
+                label={doctorInitials} 
+                color='#1db3b3'
+                labelStyle={{fontWeight: 'bold'}}
+            />
+        </View>
+    );
+ }
+
+ export class ProfileMenu extends Component {
+
+     state: {
+        isMenuVisible: boolean,
+        store: Store,
+        doctor: User,
+      };
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isMenuVisible: false,
+            store: getStore(),
+            doctor: getDoctor(),
+        };
+    }
+
+    setVisibility = (isVisible: boolean) => {
+        this.setState({
+            isMenuVisible: isVisible
+        });
+    }
+
+     renderMenuIcon() {
+        return (
+          <TouchableOpacity onPress={() => this.setVisibility(!this.props.visible)}>
+              <ProfileAvatar />
+          </TouchableOpacity>
+        );
+      }
+
+     render() {
+        return (
+        <View>
+        <Menu
+            visible={this.state.isMenuVisible}
+            onDismiss={() => this.setVisibility(false)}
+            anchor={this.renderMenuIcon()}
+            contentStyle={{top: 100 * fontScale, right: 50 * fontScale}}
+        >
+            <TouchableWithoutFeedback onPress={() => this.setVisibility(false)}>
+            <View style={{ width: 300 * fontScale, justifyContent: 'center', alignItems: 'center' }}>
+                <View style={{backgroundColor: '#eee', padding: 20 * fontScale, width: '100%', alignItems: 'center'}}>
+                    <ProfileAvatar />
+                </View>
+
+                <View style={{justifyContent: 'center', alignItems: 'center', padding: 20 * fontScale}}>
+                    <Text style={{color: '#1db3b3', fontWeight: 'bold', marginBottom: 10 * fontScale}}>
+                        {`${this.state.doctor.firstName} ${this.state.doctor.lastName}`}
+                    </Text>
+                    <Text style={{color: 'gray'}}>{`${this.state.store.name} ${this.state.store.city}`}</Text>
+                </View>
+            </View>
+            </TouchableWithoutFeedback>
+        </Menu>
+        </View>
+        );
+     }
+ }
