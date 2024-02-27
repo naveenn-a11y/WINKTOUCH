085c541249eb80a61af2eda3a45a603bda1ecf2e
spietrek <spietrek@gmail.com>
2024-02-27 12:00:39 -0500

    add css files

--- index.web.js
+++ index.web.js
@@ -2,6 +2,8 @@ import {AppRegistry} from 'react-native';
 import Index from './js/Index';
 import {name as appName} from './app.json';
 import {setIconsConfig} from './web/Util';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 AppRegistry.registerComponent(appName, () => Index);
 setIconsConfig();
