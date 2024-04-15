b0336ebb1a1756656964e657652d9d0b70f29185
spietrek <spietrek@gmail.com>
2024-04-15 10:24:33 -0400

    add support for __DEV__ only

--- js/Hosts.js
+++ js/Hosts.js
@@ -3,13 +3,20 @@ import {isWeb} from './Styles';
 import {isEmpty} from './Util';
 
 let emrHost = process.env.WINK_HOST || 'unknown';
+
 export function getEmrHost() {
   if (isTestFlight) {
     return process.env.WINK_TESTFLIGHT_HOST;
   }
-  if (isWeb && !__DEV__) {
+
+  if (__DEV__) {
+    return process.env.WINK_HOST;
+  }
+
+  if (isWeb) {
     return window.location.hostname;
   }
+
   return emrHost;
 }
 
