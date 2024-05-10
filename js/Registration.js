19cd4509436394610916fad61b9b7904dfe2383b
spietrek-insight <steve.pietrek@insight.com>
2024-05-10 13:54:30 -0400

    resolve code issues

--- js/Registration.js
+++ js/Registration.js
@@ -53,14 +53,14 @@ export let isAtWink: boolean;
 async function determineIfAtWink(): void {
   if (Platform.OS === 'web') {
       const publicIp: string = await fetchPublicIp();
-      isAtWink = publicIp === isWeb ? process.env.WINK_PUBLIC_IP : Config.WINK_PUBLIC_IP;
+      isAtWink = publicIp === process.env.WINK_PUBLIC_IP;
   }
 
   if (Platform.OS === 'ios') {
     const localIp = await fetchIp();
     if (localIp && localIp.startsWith('192.168.88.')) {
       const publicIp: string = await fetchPublicIp();
-      isAtWink = publicIp === isWeb ? process.env.WINK_PUBLIC_IP : Config.WINK_PUBLIC_IP;
+      isAtWink = publicIp === Config.WINK_PUBLIC_IP;
     } else {
       isAtWink = false;
     }
