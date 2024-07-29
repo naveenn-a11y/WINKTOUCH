0a1e8a8cd6d56b6c44831e5af99d8212f2fa19fd
spietrek-insight <steve.pietrek@insight.com>
2024-07-29 11:24:01 -0400

    add 3 new cornea code sql statements

---
+++ database/EHR-2601.sql
@@ -0,0 +1,8 @@
+INSERT INTO 6421_5tmkd.lists (list_name, description_en, description_fr, rank_en, rank_fr, rank_it, rank_es)
+VALUES ('corneaCodes.1', 'Arcus', 'Arc', NULL, NULL, NULL, NULL);
+
+INSERT INTO 6421_5tmkd.lists (list_name, description_en, description_fr, rank_en, rank_fr, rank_it, rank_es)
+VALUES ('corneaCodes.1', 'Flap Scars', 'Cicatrice de volet', NULL, NULL, NULL, NULL);
+
+INSERT INTO 6421_5tmkd.lists (list_name, description_en, description_fr, rank_en, rank_fr, rank_it, rank_es)
+VALUES ('corneaCodes.1', 'Pigment Dispersion', 'Dispersion de pigment', NULL, NULL, NULL, NULL);
