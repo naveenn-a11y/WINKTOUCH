c4c24ba8cd1e1fadd1ee5fa0e24bbe41f6c59bd3
methodmain2 <olorunfemi@downloadwink.com>
2024-09-10 13:45:26 +0100

    ipad release version and config setup

--- js/Version.js
+++ js/Version.js
@@ -12,7 +12,7 @@ export const ehrApiVersion: string = 'EHR-413';
 export const dbVersion: string = '2058';
 export const touchVersion: string = !isWeb ? DeviceInfo.getVersion() : '1';
 export const bundleVersion: string = !isWeb ? DeviceInfo.getBuildNumber() : '1';
-const MINIMAL_TOUCH_VERSION = 4.8;
+const MINIMAL_TOUCH_VERSION = 5.1;
 const EHR_VERSION_NUMBER = '4.13.1';
 
 export function setDeploymentVersion(version: string): void {
