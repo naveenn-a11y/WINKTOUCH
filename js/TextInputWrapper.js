a51d6f4ed0b4e6ad48a5a6aa144e00f8456b4e79
spietrek-insight <steve.pietrek@insight.com>
2024-09-13 11:51:14 -0400

    update textinputwrapper

--- js/TextInputWrapper.js
+++ js/TextInputWrapper.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { GestureResponderEvent, TextInput as RNTextInput, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { GestureResponderEvent, TextInput as RNTextInput, StyleSheet, Text, Pressable, View } from 'react-native';
 import { Button, TextInput as PaperTextInput, Portal } from 'react-native-paper';
 import { strings } from './Strings';
 import Dialog from './utilities/Dialog';
@@ -48,6 +48,11 @@ export const TextInputWrapper: React.FC<Props> = ({
     lastTap.current = now;
   };
 
+  const handleLongPress = () => {
+    setDialogVisible(true);
+    setTempValue(value);
+  };
+
   const handleCancel = () => {
     setTempValue(value);
     setDialogVisible(false);
@@ -58,6 +63,7 @@ export const TextInputWrapper: React.FC<Props> = ({
     setDialogVisible(false);
   };
 
+
   const renderTextInput = () => (
     <RNTextInput
       value={value}
@@ -65,7 +71,7 @@ export const TextInputWrapper: React.FC<Props> = ({
       autoCorrect={false}
       placeholder={''}
       keyboardType={keyboardType}
-      style={style}
+      style={[styles.textInput, style]}
       onFocus={onFocus}
       onChangeText={onChangeText}
       onBlur={onBlur}
@@ -88,9 +94,9 @@ export const TextInputWrapper: React.FC<Props> = ({
 
   return (
     <View style={{ width: '100%', height: '100%'}}>
-      <TouchableWithoutFeedback onPress={handleDoubleClick}>
+      <Pressable onLongPress={handleLongPress} delayLongPress={500}>
         {renderTextInput()}
-      </TouchableWithoutFeedback>
+      </Pressable>
 
       <Portal theme={{colors: {backdrop: 'transparent'}}}>
         <Dialog
@@ -132,6 +138,7 @@ export const TextInputWrapper: React.FC<Props> = ({
 const styles = StyleSheet.create({
   textInput: {
     width: '100%',
+    height: '100%',
   },
 });
 
