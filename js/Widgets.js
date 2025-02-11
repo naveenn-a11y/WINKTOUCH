6ea0b9d40bde33770757606cb5a69b9a9b05fa99
Kanishk Gupta <Kanishk.gupta@insight.com>
2025-02-11 22:26:16 +0530

    Null value Fix
    Removed CloneDeep

--- js/Widgets.js
+++ js/Widgets.js
@@ -1450,10 +1450,10 @@ export class TilesField extends Component {
       return;
     }
 
-    let filteredValue = deepClone(this.props.value);
+    let filteredValue = this.props.value;
     if((this.props.prefix instanceof Array && this.props?.prefix?.length > 0 && typeof filteredValue === 'string')){
-        filteredValue = filteredValue?.replace(this.props.prefix?.[0], '');
-      }
+      filteredValue = filteredValue?.replace(this.props.prefix?.[0], '');
+    }
 
     this.setState({
       isActive: true,
