7a934ce001fda49063f6b706a3c91d5e7ad4dd79
Kanishk Gupta <Kanishk.gupta@insight.com>
2025-02-12 13:42:43 +0530

    Removed Optional Chaining from this.props.prefix

--- js/Widgets.js
+++ js/Widgets.js
@@ -1452,7 +1452,7 @@ export class TilesField extends Component {
 
     let filteredValue = this.props.value;
     if((this.props.prefix instanceof Array && this.props?.prefix?.length > 0 && typeof filteredValue === 'string')){
-      filteredValue = filteredValue?.replace(this.props.prefix?.[0], '');
+      filteredValue = filteredValue?.replace(this.props.prefix[0], '');
     }
 
     this.setState({
