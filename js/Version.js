e62ecc366f46eea80be0ae655ac690da56ce7411
Ali Chakavak <ali.c@winkdownload.com>
2025-04-02 01:18:23 -0400

    new release version change

--- js/Version.js
+++ js/Version.js
@@ -10,7 +10,7 @@ import { isWeb } from './Styles';
 export const deploymentVersion: string = 'v413';
 export const ehrApiVersion: string = 'EHR-413';
 export const dbVersion: string = '2060';
-export const touchVersion: string = !isWeb ? DeviceInfo.getVersion() : '4';
+export const touchVersion: string = !isWeb ? DeviceInfo.getVersion() : '5';
 export const bundleVersion: string = !isWeb ? DeviceInfo.getBuildNumber() : '1';
 const MINIMAL_TOUCH_VERSION = 5.0;
 const EHR_VERSION_NUMBER = '4.13.4';
